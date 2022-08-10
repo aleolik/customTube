bfa21e5ea2827feee6c65cc9bc8ac8099f0d6f95
aleolik <sashaoleyynik8@gmail.com>
2022-08-10 15:56:21 +0300

    added useLoading hooks,upgraded profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -15,69 +15,84 @@ const ProfilePage = () => {
   const {username} = useParams<string>()
 
   const getUsersPage = useGetUserData(username)
-  const [usersPage,setUsersPage] = useState<IUser | null | undefined>(null)
 
   const user = useAppSelector(state => state.user.user)
 
   // load videos
   const {error,loading,videos} = useAppSelector(state => state.video)
   // load usersPage
-  const useLoad = useLoading()
+  const [usersPage,setUsersPage] = useState<null | undefined | IUser>(null)
+  const [fetchData,fetchError,fetchLoading] = useLoading(async() => {
+    await getUsersPage.then((res) => setUsersPage(res?.user))
+  })
 
   useEffect(() => {
     if (username){
       dispatch(LoadUserVideos(username))
-      const data = getUsersPage.then((res) => {
-        setUsersPage(res?.user)
-      })
+      fetchData()
+      
     }
   },[username])
 
   return (
     <div className={css.center}>
-      {usersPage !== null && usersPage!== undefined ? (
-            <div>
-                <div style={{'display':'flex'}}>
-                  <h4 style={{'marginTop':20+'px','border':'black 5px solid'}}>{usersPage?.username}</h4>
-                </div>
-                {usersPage?.photoUrl === null
-                ? (
-                  <img alt='avatar' src={defaultUserAvatar} style={{'width':100+'px','height':60+'px','borderRadius' : 60+'px','marginTop':10+'px','border':'2px solid gray'}}></img>
-                )
-                  : (
-                    <img  alt='avatar' src={usersPage?.photoUrl} style={{'width':100+'px','height':60+'px','borderRadius' : 60+'px','marginTop':10+'px','border':'2px solid gray'}} ></img>
-                )}
-                {user?.username === username && (
-          <VideoForm/>
-        )}
-         {loading
-         ? (<div>Загрузка...</div>)
-         : (
-          <div>
-            {videos.length
-            ? (
-              <div className={css.videos}>
-                {videos.map((video,index) => {
-                  return(
-                    <div className={css.video} key={video.id}>
-                      <h3>
-                          {video.name}
-                       </h3>
+      {error && (
+        <div>{error}</div>
+      )}
+      {fetchError && (
+        <div>{fetchError}</div>
+      )}
+      {fetchLoading && !fetchError && !error
+      ? (
+        <div>Загрузка</div>
+      )
+      : (
+        <div>
+            {usersPage !== null && usersPage!== undefined ? (
+                <div>
+                    <div style={{'display':'flex'}}>
+                      <h4 style={{'marginTop':20+'px','border':'black 5px solid'}}>{usersPage?.username}</h4>
                     </div>
-                  )
-                })}
-              </div>
-            )
-            : (
-              <div>Ошибка</div>
+                    {usersPage?.photoUrl === null
+                    ? (
+                      <img alt='avatar' src={defaultUserAvatar} style={{'width':100+'px','height':60+'px','borderRadius' : 60+'px','marginTop':10+'px','border':'2px solid gray'}}></img>
+                    )
+                      : (
+                        <img  alt='avatar' src={usersPage?.photoUrl} style={{'width':100+'px','height':60+'px','borderRadius' : 60+'px','marginTop':10+'px','border':'2px solid gray'}} ></img>
+                    )}
+                    {user?.username === username && (
+              <VideoForm/>
             )}
-          </div>
-         )}
-          </div>
-    )
-    : (
-      <NotFoundPage/>
-    )}
+            {loading
+            ? (<div>Загрузка...</div>)
+            : (
+              <div>
+                {videos.length
+                ? (
+                  <div className={css.videos}>
+                    {videos.map((video,index) => {
+                      return(
+                        <div className={css.video} key={video.id}>
+                          <h3>
+                              {video.name}
+                          </h3>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
+                : (
+                  <div>Ошибка</div>
+                )}
+              </div>
+            )}
+              </div>
+        )
+        : (
+          <NotFoundPage/>
+        )}    
+        </div>
+      )}
     </div>
   )
 }
