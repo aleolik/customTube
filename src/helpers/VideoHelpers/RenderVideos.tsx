02c2962db18c76082b2e6df384a848e8c2018cf2
aleolik <sashaoleyynik8@gmail.com>
2023-02-03 10:46:18 +0200

    try commit

--- src/helpers/VideoHelpers/RenderVideos.tsx
+++ src/helpers/VideoHelpers/RenderVideos.tsx
@@ -1,16 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Card } from '../../components/Card'
 import { IVideo } from '../../types/VideoTypes'
+import { isDesktop } from 'react-device-detect'
 
 interface RenderVideosProps{
     videos : IVideo[]
 }
+
 const RenderVideos : FC<RenderVideosProps> = ({videos}) => {
+
+  useEffect(() => {
+    console.log(window.innerWidth)
+  },[])
+
   return (
     <div className='row'>
         {videos.map((video) => {
             return(
-             <div  className='col-lg-2 col-md-6 col-sm-6' key={video.id}>
+             <div className={`${window.innerWidth > 1600 ? 'col-lg-2 col-md-4 col-sm-6' : 'col-lg-3 col-md-4 col-sm-6'}`} key={video.id}>
                 <Card video={video}/>
              </div>
             )
